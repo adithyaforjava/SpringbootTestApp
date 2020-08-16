55577cd424871db46fd417f30d0f7c1ab00ab4be
adithyaforjava <adithyaforjava@gmail.com>
2020-08-16 23:33:07 +0530

    Modified Message Runner

--- src/main/java/in/nit/adithya/controller/MessageRestController.java
+++ src/main/java/in/nit/adithya/controller/MessageRestController.java
@@ -2,6 +2,7 @@ package in.nit.adithya.controller;
 
 import java.util.Random;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
