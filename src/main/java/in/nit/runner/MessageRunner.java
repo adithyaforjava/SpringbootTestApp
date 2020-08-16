6f8cefc74ad8d1a2bc1e1a74f01e75e6dc0e7a8e
adithyaforjava <adithyaforjava@gmail.com>
2020-08-16 23:28:43 +0530

    Added MessageRunner.java to application

---
+++ src/main/java/in/nit/runner/MessageRunner.java
@@ -0,0 +1,15 @@
+package in.nit.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+        
+		
+		System.out.println("Welcmoe to My App");
+
+	}
+
+}
